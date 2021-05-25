0b0241a227cda283df87a42081b1ea91a39beba0
fmacca <48057744+fmacca@users.noreply.github.com>
2021-05-25 18:52:04 +0200

    Correction of bias_analitic sign

--- Resources/bias_analytic.m
+++ Resources/bias_analytic.m
@@ -9,7 +9,7 @@ H_aa=zeros(N,N,N); % third index refers to corresponding root
 for ii=1:N
     r_=r(ii).^((N-1:-1:0)');
     r_p=((N-1:-1:0)').*r(ii).^((N-2:-1:-1)');
-    H_aa(:,:,ii)=-1/(polyval(deriv,r(ii)))^2*(r_*conj(r_p')+r_p*conj(r_')-polyval(deriv2,r(ii))/polyval(deriv,r(ii))*r_*conj(r_'));
+    H_aa(:,:,ii)=1/(polyval(deriv,r(ii)))^2*(r_*conj(r_p')+r_p*conj(r_')-polyval(deriv2,r(ii))/polyval(deriv,r(ii))*r_*conj(r_'));
 end
 % Computation of the bias
 C=Sigma(1:N,(N+1):(2*N));
