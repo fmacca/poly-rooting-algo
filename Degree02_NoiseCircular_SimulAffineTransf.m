871a009230b2342de43affd6d0d1599e44359c57
fmacca <48057744+fmacca@users.noreply.github.com>
2021-05-31 18:07:48 +0200

    Updates

--- Degree02_NoiseCircular_SimulAffineTransf.m
+++ Degree02_NoiseCircular_SimulAffineTransf.m
@@ -14,7 +14,7 @@ results_folder=strcat(folder_name,'/',currDate);
 
 %% Parameters
 N=2; % order of the polynomial
-sigma_a=.1; % variance of the noise on coefficients
+sigma_a=0.05; % variance of the noise on coefficients
 K=10^4; % Number of iterations per simulation (n of noisy measurements per polynomial)
 scale=2; % the roots of the polynomial will be generated with Re(r),Im(r) in [-scale +scale], a square
 
@@ -22,9 +22,11 @@ scale=2; % the roots of the polynomial will be generated with Re(r),Im(r) in [-s
 % Generate a random circular covariance
 [Sigma,C_atilda,A] = generate_covariance(N,sigma_a,'circular');
 % Generate "roots of 1" polynomial
-a=[1; 0; 1];
+a=[1.0000 + 0.0000i;   2.5880 - 0.3628i;   1.6431 - 0.4871i];
 % Generate random roots
 r=roots(a);
+[a_transf,~]=poly2D_affinetransf(a);
+r_transf=roots(a_transf);
 
 J=[eye(N) 1i*eye(N);eye(N) -1i*eye(N)]; % Notation change matrix
 
@@ -35,19 +37,23 @@ a_n_transf=zeros(N+1,K); %Matrix to contain transformed coefficients at every it
 t=zeros(K); %Vector to contain the affine shift at every iteration
 r_n=zeros(N,K); %Matrix to collect roots computed at every iteration
 r_n_transf=zeros(N,K); %Matrix to collect roots computed from transformation at every iteration
-err_n=zeros(N,K); %Matrix to collect the error in roots at every step
+r_n_retransf=zeros(N,K); %Matrix to collect roots retrasformed back
+% err_n=zeros(N,K); %Matrix to collect the error in roots at every step
 for k=1:K
     noise_tilda=A*randn(2*N,1); %Generate colored noise
     a_n(:,k)=a+[0;noise_tilda(1:N)+1i*noise_tilda(N+1:2*N)]; %Add noise to coefficients
     [a_n_transf(:,k),t(k)] = poly2D_affinetransf(a_n(:,k)); %Collect the transformed polynomials
     r_curr=roots(a_n_transf(:,k)); %Compute the roots
     r_n_transf(:,k)=r_curr(order_roots_permutations(r_curr,r)); %Save roots ordered w.r.t. original roots
-    r_n(:,k)=t(k)+r_n_transf(:,k); %Add the shift to reobtain original roots
-    err_n(:,k)=r_n(:,k)-r;
+    r_n_retransf(:,k)=t(k)+r_n_transf(:,k); %Add the shift to reobtain original roots
+    r_curr=roots(a_n(:,k)); %Compute the roots
+    r_n(:,k)=r_curr(order_roots_permutations(r_curr,r)); %Roots computed without transformation
+%     err_n(:,k)=r_n(:,k)-r;
 
     waitbar(k/K) %Update waitbar
 end
 r_mean = mean(r_n,2); %Mean of the roots computed at every iteration
+r_transf_mean = mean(r_n_transf,2); %Mean of the roots computed at every iteration
 
 close(h); %Close waitbar
 
@@ -64,7 +70,7 @@ for ii=1:N
     ellipse_plot(0.1*inv(C_atilda([ii N+ii],[ii N+ii])),[real(a(1+ii)),imag(a(1+ii))])
 end
 plot(real(a),imag(a),'*k','MarkerSize',20);
-axis equal;axis(1.5*[-1,1,-1,1]);
+axis equal;axis(5*[-1,1,-1,1]);
 title("Coefficients of original polynomial");grid on;hold off
 
 subplot(2,3,2);
@@ -76,8 +82,8 @@ end
 % for ii=1:N
 %     ellipse_plot(0.1*inv(C_atilda([ii N+ii],[ii N+ii])),[real(a(1+ii)),imag(a(1+ii))])
 % end
-plot(real(a),imag(a),'*k','MarkerSize',20);
-axis equal;axis(1.5*[-1,1,-1,1]);
+plot(real(a_transf),imag(a_transf),'*k','MarkerSize',20);
+axis equal;axis(5*[-1,1,-1,1]);
 title("Coefficients of transformed polynomial");grid on;hold off
 
 subplot(2,3,3);
@@ -88,7 +94,7 @@ plot(real(t),imag(t),'.','MarkerSize',1); hold on; % Simulated coeff
 %     ellipse_plot(0.1*inv(C_atilda([ii N+ii],[ii N+ii])),[real(a(1+ii)),imag(a(1+ii))])
 % end
 % plot(real(a),imag(a),'*k','MarkerSize',20);
-axis equal;axis(1.5*[-1,1,-1,1]);
+axis equal;axis(5*[-1,1,-1,1]);
 title("Shift paramenter t");grid on;hold off
 
 subplot(2,3,4);
@@ -101,12 +107,27 @@ end
 % %     Ellipse_plot(0.1*inv(Cov_ztilda([ii N+ii],[ii N+ii])),[real(avgs(ii))-real(bias(ii)),imag(avgs(ii))-imag(bias(ii))])
 %     ellipse_plot(0.1*inv(MSE_analytic_tilda([ii N+ii],[ii N+ii])),[real(r(ii)),imag(r(ii))])
 % end
-plot(real(r_mean),imag(r_mean),'.b','MarkerSize',15); % Mean of estimated roots
-plot(real(r),imag(r),'*k','MarkerSize',20); % True roots
-axis equal;axis(1.5*[-1,1,-1,1]);
+plot(real(r_transf_mean),imag(r_transf_mean),'.b','MarkerSize',15); % Mean of estimated roots
+plot(real(r_transf),imag(r_transf),'*k','MarkerSize',20); % True roots
+axis equal;axis(5*[-1,1,-1,1]);
 title("Roots of transformed polynomial");grid on;hold off
 
 subplot(2,3,5);
+viscircles([0 0],1,'color','b','linestyle','--','LineWidth',0.1);hold on;
+plot(zeros(2,1),5*[-1,1],'b--','LineWidth',0.1);plot(5*[-1,1],zeros(2,1),'b--','LineWidth',0.1);
+for ii=1:N
+    plot(real(r_n_retransf(ii,:)),imag(r_n_retransf(ii,:)),'.','MarkerSize',1); hold on; % Simulated roots
+end
+% for ii=1:N
+% %     Ellipse_plot(0.1*inv(Cov_ztilda([ii N+ii],[ii N+ii])),[real(avgs(ii))-real(bias(ii)),imag(avgs(ii))-imag(bias(ii))])
+%     ellipse_plot(0.1*inv(MSE_analytic_tilda([ii N+ii],[ii N+ii])),[real(r(ii)),imag(r(ii))])
+% end
+plot(real(r_mean),imag(r_mean),'.b','MarkerSize',15); % Mean of estimated roots
+plot(real(r),imag(r),'*k','MarkerSize',20); % True roots
+axis equal;axis(5*[-1,1,-1,1]);
+title("Roots transformed back");grid on;hold off
+
+subplot(2,3,6);
 viscircles([0 0],1,'color','b','linestyle','--','LineWidth',0.1);hold on;
 plot(zeros(2,1),5*[-1,1],'b--','LineWidth',0.1);plot(5*[-1,1],zeros(2,1),'b--','LineWidth',0.1);
 for ii=1:N
@@ -118,7 +139,7 @@ end
 % end
 plot(real(r_mean),imag(r_mean),'.b','MarkerSize',15); % Mean of estimated roots
 plot(real(r),imag(r),'*k','MarkerSize',20); % True roots
-axis equal;axis(1.5*[-1,1,-1,1]);
+axis equal;axis(5*[-1,1,-1,1]);
 title("Roots of the original polynomial");grid on;hold off
 
 %% Save workspace and figures to the folder
